7a9b27a6bcd88e33f6a4fafde3d710000e720c6d
deepmind0240 <105158334+deepmind0240@users.noreply.github.com>
2022-09-18 19:57:08 +0300

    react-netflix-clone
    tmdb api demo

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '032c49ef1e6df24af7189b8ffedc38cf';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
